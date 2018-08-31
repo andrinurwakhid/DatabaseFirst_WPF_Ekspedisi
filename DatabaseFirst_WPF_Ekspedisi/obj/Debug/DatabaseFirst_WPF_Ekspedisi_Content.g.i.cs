88030d2c9f8c05dd23b41cc1d359fe98916c6bd9
Andri Nur Wakhid <andriwakhid01@gmail.com>
2018-08-31 17:02:35 +0700

    login

--- DatabaseFirst_WPF_Ekspedisi/obj/Debug/DatabaseFirst_WPF_Ekspedisi_Content.g.i.cs
+++ DatabaseFirst_WPF_Ekspedisi/obj/Debug/DatabaseFirst_WPF_Ekspedisi_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/admin.jpg")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("models/model1.context.tt")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("models/model1.tt")]
 
